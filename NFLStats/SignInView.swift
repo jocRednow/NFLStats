42693e4a50ae0b9275e9742c67eda11fd4f7b511
stefankrasnow <jocrednow@gmail.com>
2024-08-21 10:43:23 +0200

    signInView mock

---
+++ NFLStats/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  NFLStats
+//
+//  Created by Stepan Krasnov on 20/08/2024.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
